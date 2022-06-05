9698f1ff2ae90b4dd88a690bf35e9f91dd292495
rickie maweu <rickiemaweu@gmail.com>
2022-06-05 20:13:48 +0300

    commit tree 99e59eb9b958add6a18491c9b5998c744fe33d89

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+// Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FF69B4";
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+        console.log('hey')
+        moveDodgerLeft();
+    }
+  });
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowRight") {
+        console.log('hey')
+        moveDodgerRight();
+    }
+  });
+
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+      console.log(left)
+    }
+  }
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+      console.log(left)
+    }
+  }
